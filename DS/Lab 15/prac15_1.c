e0b89ee5f3a17fc0caebbc3a19181a9683ed1d3f
PriyaKhakhar <workpriyakhakhkhar@gmail.com>
2026-09-17 10:30:54 +0530

    Update prac15_1.c

--- DS/Lab 15/prac15_1.c
+++ DS/Lab 15/prac15_1.c
@@ -1,4 +1,5 @@
 // 53. WAP to copy a linked list. Take 2 linked lists
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -45,4 +46,62 @@ void copy(struct node *first1)
     }
 
     printf("Linked list copied successfully\n");
+}
+
+void main()
+{
+    struct node *newnode, *temp;
+    int size, i;
+
+    printf("Enter size of first linked list: ");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++)
+    {
+        newnode = (struct node *)malloc(sizeof(struct node));
+
+        printf("Enter element: ");
+        scanf("%d", &newnode->info);
+
+        newnode->link = NULL;
+
+        if(FIRST1 == NULL)
+        {
+            FIRST1 = newnode;
+            temp = newnode;
+        }
+        else
+        {
+            temp->link = newnode;
+            temp = newnode;
+        }
+    }
+
+    printf("First linked list created successfully\n");
+
+    copy(FIRST1);
+
+    printf("Elements of first linked list:\n");
+
+    temp = FIRST1;
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->info);
+        temp = temp->link;
+    }
+
+    printf("\n");
+
+    printf("Elements of second linked list:\n");
+
+    temp = FIRST2;
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->info);
+        temp = temp->link;
+    }
+
+    printf("\n");
 }
